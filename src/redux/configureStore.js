ab2ec5a0b95a2bdbfd740b9ba15926206b6ba7e6
Radeep <radeep.et@cognizant.com>
2021-07-19 10:51:29 +0530

    Add files via upload

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,23 @@
-import { combineReducers, createStore } from 'redux'
+import { combineReducers, createStore, applyMiddleware, compose } from 'redux'
+import createSagaMiddleware from 'redux-saga';  //create saga middle ware
 import counterReducer from './reducer/counter'
+import userReducer from './reducer/user'
+import { watcherSaga } from './sagas/rootSaga';
 
 const reducer = combineReducers({
-    counter: counterReducer
+    counter: counterReducer,
+    user: userReducer
 });
 
-const store = createStore(reducer);
+const composeEnhancers =
+window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const sagaMiddleware = createSagaMiddleware();
+
+const middleware = [sagaMiddleware]; //create array of middleware require, here sagamiddleware, logmiddleware etc
+
+const store = createStore(reducer,{}, composeEnhancers(applyMiddleware(...middleware))); //second parameter enhancer
+
+sagaMiddleware.run(watcherSaga); //runs consatanty in the background and listens for the any redux functions
 
 export default store;
